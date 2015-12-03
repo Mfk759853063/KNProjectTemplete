632596f5b92adb3200790771769002fe8c202588
kwep_vbn <vbn@candeMacBook-Pro.local>
2015-12-03 12:48:41 +0800

    项目模板

--- KNProjectTemplete/ViewController.m
+++ KNProjectTemplete/ViewController.m
@@ -17,7 +17,7 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
-    self.kn_navigationBar.titleLabel.text = @"Hello World";
+    self.kn_navigationBar.title = @"Hello World";
     self.view.backgroundColor = [UIColor redColor];
 }
 
