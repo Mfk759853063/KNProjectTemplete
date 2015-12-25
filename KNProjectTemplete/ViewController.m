b721fc4ec1ff520450f4fc37ca6d42f667712ce4
周灿 <6228851057209164>
2015-12-25 15:44:33 +0800

    commit

--- KNProjectTemplete/ViewController.m
+++ KNProjectTemplete/ViewController.m
@@ -19,6 +19,20 @@
     // Do any additional setup after loading the view, typically from a nib.
     self.kn_navigationBar.title = @"Hello World";
     self.view.backgroundColor = [UIColor redColor];
+    
+    dispatch_after(dispatch_time(DISPATCH_TIME_NOW, (int64_t)(2 * NSEC_PER_SEC)), dispatch_get_main_queue(), ^{
+        UIViewController *vc = [[ViewController alloc] init];
+        [self.navigationController pushViewController:vc animated:YES];
+    });
+    
+    if (1) {
+        UIImage *backImage = [UIImage imageNamed:@"bton_back_normal"];
+        __weak ViewController *weakSelf = self;
+        self.kn_navigationBar.leftItem = [[KNNavigationItem alloc] initWithItemSize:CGSizeMake(100, 44) title:@"" image:backImage handler:^(id sender) {
+            [weakSelf.navigationController popViewControllerAnimated:YES];
+        }];
+    }
+
 }
 
 - (void)didReceiveMemoryWarning {
